689b7d600318cb38d56a055ae0126bc821ef1c38
angelTG-dev <angeljtg1@gmail.com>
2025-09-28 14:12:05 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: 'Hello World'
+    })
+}) 
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
